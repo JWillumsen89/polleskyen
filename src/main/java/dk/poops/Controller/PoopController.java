77a4b23bdd266228ea0b985361f23806693a6e7f
Ramilade <rami0904@stud.kea.dk>
2022-10-24 12:08:09 +0200

    Controller, index og css

---
+++ src/main/java/dk/poops/Controller/PoopController.java
@@ -0,0 +1,12 @@
+package dk.poops.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PoopController {
+  @GetMapping("/")
+  private String index(){
+    return "index";
+  }
+}
