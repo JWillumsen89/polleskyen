d2a92f6dbea66a08eca8b96be49ecd9b7f89c40f
JWillumsen89 <100369030+JWillumsen89@users.noreply.github.com>
2022-10-24 11:29:38 +0200

    Initial commit

---
+++ src/main/java/dk/poops/poopcloud/PoopcloudApplication.java
@@ -0,0 +1,13 @@
+package dk.poops.poopcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoopcloudApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PoopcloudApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dk/poops/poopcloud/PoopcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.poops.poopcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoopcloudApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
