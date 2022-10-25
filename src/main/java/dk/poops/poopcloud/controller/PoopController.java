395a4c97ad9fecf1069f3a0b25a0a17a3ee9199c
FHallengreen <fred70p9@stud.kea.dk>
2022-10-25 18:51:31 +0200

    corrected stylesheet

--- src/main/java/dk/poops/poopcloud/controller/PoopController.java
+++ src/main/java/dk/poops/poopcloud/controller/PoopController.java
@@ -7,10 +7,7 @@ import dk.poops.poopcloud.service.WishService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PoopController {
@@ -65,6 +62,7 @@ public class PoopController {
     return "redirect:/";
   }
 
+  @RequestMapping("/")
   @GetMapping("/showwishlist/createwish/{id}")
   public String showWishCreateForm(@PathVariable("id")int id, Model model){
     model.addAttribute("wishlist",wishListService.findWishListById(id));
