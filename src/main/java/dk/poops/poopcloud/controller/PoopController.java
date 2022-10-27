6f37b4821d833da525fc7a336c3e971c17bb4881
FHallengreen <fred70p9@stud.kea.dk>
2022-10-27 17:02:41 +0200

    changes to header layout

--- src/main/java/dk/poops/poopcloud/controller/PoopController.java
+++ src/main/java/dk/poops/poopcloud/controller/PoopController.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 @Controller
 public class PoopController {
 
